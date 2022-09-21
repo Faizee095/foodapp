763a9ef35e5f2402cb05c89203c029519d7822c8
Faizee095 <faiyazsiddique9@gmail.com>
2022-09-22 00:25:30 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="w-screen h-screen flex items-center justify-center text-blue-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
